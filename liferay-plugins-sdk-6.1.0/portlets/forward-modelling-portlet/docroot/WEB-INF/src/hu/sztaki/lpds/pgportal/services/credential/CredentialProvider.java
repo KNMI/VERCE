b1082c9ce200ed2b62dda7fafda67f45f83e4e38
Ramos <Ramos@e3b179e7-38d4-40e1-96b2-8a60d5dcc249>
2014-01-07 11:16:29 +0000

    minor. Prepare if we want to catch the exception
    git-svn-id: http://sisprojs.ipgp.fr/repos/verce/All/SA/SA3/Scientifc-Gateway/sg-trunk@3025 e3b179e7-38d4-40e1-96b2-8a60d5dcc249

--- liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/WEB-INF/src/hu/sztaki/lpds/pgportal/services/credential/CredentialProvider.java
+++ liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/WEB-INF/src/hu/sztaki/lpds/pgportal/services/credential/CredentialProvider.java
@@ -1,5 +1,10 @@
 package hu.sztaki.lpds.pgportal.services.credential;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.*;
+
 import hu.sztaki.lpds.pgportal.services.asm.ASMCredentialProvider;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -26,8 +31,33 @@ public class CredentialProvider {
     String pUser, @WebParam(name = "pMiddleware")
     String pMiddleware, @WebParam(name = "pVo")
     String pVo) throws Exception {
-        ASMCredentialProvider asmcred = new ASMCredentialProvider();
-        return asmcred.get(pUser, pVo);
+    	try{
+	        ASMCredentialProvider asmcred = new ASMCredentialProvider();
+	        byte[] result = asmcred.get(pUser, pVo);
+	        return result;
+    	}
+    	catch (Exception e)
+    	{
+    		/*Format formatter = new SimpleDateFormat("yyyyMMddHHmm");
+    		String text = "Exception caught in CredentialProvider.java\n";
+    		text += pUser+" "+pVo+" "+formatter.format(new Date())+"\n";
+    		text += e.getMessage()+"\n";
+    		text += e.getStackTrace()+"\n";
+    		String fileName = "logs/credentialprovider/cp_errorfile_"+pUser+"_"+formatter.format(new Date())+".txt";
+            
+            Writer writer = null;
+            try {
+                writer = new BufferedWriter(new OutputStreamWriter(
+                      new FileOutputStream(fileName), "utf-8"));
+                writer.write(text);
+            } catch (IOException ex) {
+              // report
+            } finally {
+               try {writer.close();} catch (Exception ex) {}
+            }
+    		System.out.println("[CredentialProvider] He pillat l'excepci—!! "+pVo);*/
+    		throw e;
+    	}
 
     }
 
