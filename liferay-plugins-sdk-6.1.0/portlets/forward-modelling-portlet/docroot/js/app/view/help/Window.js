9fe6a8e96c6d4a7242fa3e529bfd24df1b6902ad
Ramos <Ramos@e3b179e7-38d4-40e1-96b2-8a60d5dcc249>
2014-03-06 10:23:27 +0000

    Help page
    git-svn-id: http://sisprojs.ipgp.fr/repos/verce/All/SA/SA3/Scientifc-Gateway/sg-trunk@3210 e3b179e7-38d4-40e1-96b2-8a60d5dcc249

--- liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/view/help/Window.js
+++ liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/view/help/Window.js
@@ -2,6 +2,10 @@
  * Help Window with static content using 'contentEl' property.
  * @extends Ext.window.Window
  */
+
+var helpIframe = '<iframe id="test_iframe" src="http://www.verce-project.eu/projects/docu1/wiki/HPC_use-case?parent=User_documentation" width="1000px" height="600px"></iframe>';
+var helpTitle = 'Help';
+
 Ext.define('CF.view.help.Window', {
     extend: 'Ext.window.Window',
     alias : 'widget.cf_helpwindow',
@@ -10,8 +14,9 @@ Ext.define('CF.view.help.Window', {
             bodyCls: "cf-helpwindow",
             closeAction: "hide",
             layout: 'fit',
-            maxWidth: 600,
-            title: "Help"
+            maxWidth: 1200,
+            html: helpIframe,
+            title: helpTitle
         });
         this.callParent(arguments);
     }
