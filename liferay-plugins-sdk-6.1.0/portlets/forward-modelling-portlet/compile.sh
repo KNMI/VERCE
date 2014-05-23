beb930d9c4fc02ff2566a082c7875c9092e732d8
matser <matser@e3b179e7-38d4-40e1-96b2-8a60d5dcc249>
2014-05-23 10:01:11 +0000

    Remove arbor from compilation
    git-svn-id: http://sisprojs.ipgp.fr/repos/verce/All/SA/SA3/Scientifc-Gateway/sg-trunk@3821 e3b179e7-38d4-40e1-96b2-8a60d5dcc249

--- liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/compile.sh
+++ liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # requires Sencha Cmd installed and in the path
-sencha -sdk ./ext-4.2.1.883/ compile -classpath=./docroot/js/_,./docroot/js/lib/arbor.js,./docroot/js/app.js,./docroot/js/app,./docroot/js/src/GeoExt concatenate -st -co ./docroot/js/app-all.js
+# don't compile arbor and related libraries, because of webworkers
+sencha -sdk ./ext-4.2.1.883/ compile -classpath=./docroot/js/app.js,./docroot/js/app,./docroot/js/src/GeoExt concatenate -st -co ./docroot/js/app-all.js
 
 # To actually use the compiled file, see mapgui.jsp
