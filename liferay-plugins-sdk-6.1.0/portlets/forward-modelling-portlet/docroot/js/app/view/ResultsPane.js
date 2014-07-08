405c9ea31f65537494171990c6ad04943e7a5cc0
Spinuso <Spinuso@e3b179e7-38d4-40e1-96b2-8a60d5dcc249>
2014-07-08 19:04:57 +0000

    prserved fields values on run search
    git-svn-id: http://sisprojs.ipgp.fr/repos/verce/All/SA/SA3/Scientifc-Gateway/sg-trunk@4019 e3b179e7-38d4-40e1-96b2-8a60d5dcc249

--- liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/view/ResultsPane.js
+++ liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/view/ResultsPane.js
@@ -1,7 +1,241 @@
- 
+var activityStore = Ext.create('CF.store.Activity');
+
+var artifactStore = Ext.create('CF.store.Artifact');
+
+var singleArtifactStore = Ext.create('CF.store.Artifact');
+
+var workflowStore = Ext.create('CF.store.ProvWorkflow');
+
+var workflowInputStore = Ext.create('CF.store.WorkflowInput');
+
 
 // specifies the userhome of whom we are going to access the data from (for sharing purposes)
 owner=userSN
+
+Ext.define('CF.view.WorlflowSelection', {
+
+  width: 780,
+  disableSelection: true,
+
+  autoHeight: true,
+  extend: 'Ext.grid.Panel',
+
+
+  requires: [
+    'CF.store.Workflow',
+    'Ext.grid.plugin.BufferedRenderer'
+  ],
+  store: workflowStore,
+
+
+  /*   verticalScroller: {
+        xtype: 'paginggridscroller'
+    },*/
+
+
+
+  /* plugins: [{
+        ptype: 'bufferedrenderer'
+    }],*/
+
+
+  /* selModel: {
+        pruneRemoved: false
+    },
+*/
+  initComponent: function() {
+    Ext.apply(this, {
+      border: false,
+
+      loadMask: true,
+       
+      columns: [{
+          xtype: 'rownumberer',
+
+          sortable: false
+        },
+
+        {
+          header: 'Run ID',
+          dataIndex: 'runId',
+          flex: 5,
+          sortable: false
+        }, {
+          header: 'Workflow Name',
+          dataIndex: 'workflowName',
+          flex: 3,
+          sortable: false,
+          groupable: false
+        }, {
+          header: 'Description',
+          dataIndex: 'description',
+          flex: 3,
+          sortable: false,
+          field: {
+            xtype: 'textfield',
+            allowBlank: true,
+            maxLength: 50
+          }
+        }, {
+          header: 'Date',
+          dataIndex: 'date',
+          flex: 3,
+          sortable: false,
+          groupable: false
+        }, // custom mapping
+        {
+          xtype: 'actioncolumn',
+          width: 40,
+          tdCls: 'Delete',
+          items: [{
+            icon: localResourcesPath + '/img/delete-icon.png', // Use a URL in the icon config
+            tooltip: 'Delete',
+            handler: function(grid, rowIndex, colIndex) {
+              var rec = grid.getStore().getAt(rowIndex);
+
+              var storeClassName = Ext.getClassName(grid.getStore());
+              var tempStore = Ext.create(storeClassName, {
+                buffered: false
+              });
+              tempStore.add(rec);
+              var xx = tempStore.getAt(0)
+              messagebox = Ext.Msg.confirm('Remove Run', 'Are you sure?', function(button) {
+                if (button == 'yes') {
+
+
+                  var tempx = tempStore.getProxy()
+                  tempx.api.destroy = PROV_SERVICE_BASEURL + "workflow/delete/" + xx.get("runId");
+
+                  tempStore.remove(xx);
+
+                  tempStore.sync({
+                    success: function(args) {
+
+
+                      Ext.Ajax.request({
+                        url: deleteWorkflowURL,
+                        params: {
+                          "workflowId": xx.get('systemId')
+                        },
+                        success: function(response) {
+                          wfStore.load();
+                        },
+                        failure: function(response) {
+
+                        }
+                      })
+
+                      // workflowStore.data.clear()
+                      workflowStore.load()
+
+
+                    },
+
+                    faliure: function(args) {
+
+                      Ext.Msg.alert("Error", "Delete failed!");
+
+                    }
+
+                  })
+
+
+
+                }
+              })
+              messagebox.zIndexManager.bringToFront(messagebox);
+
+
+
+            }
+
+
+
+          }]
+        }
+      ],
+      flex: 1,
+      selType: 'cellmodel',
+      plugins: [
+        Ext.create('Ext.grid.plugin.CellEditing', {
+          clicksToEdit: 1,
+          listeners: {
+            'validateedit': function(c, e, eOpts) {
+              var r = e.rowIdx;
+              var sr = workflowStore.getAt(r);
+
+              sr.set(e.field, e.value);
+
+              workflowStore.load()
+            }
+          }
+        })
+      ]
+
+
+
+
+    });
+    this.callParent(arguments);
+
+    // store singleton selection model instance
+
+
+  },
+
+  viewConfig: {
+     enableTextSelection: true,
+    listeners: {
+   
+      itemclick: function(dv, record, item, index, e) {
+        workflowStore.sync()
+      },
+      itemdblclick: function(dataview, record, item, index, e) {
+
+        activityStore.setProxy({
+          type: 'ajax',
+          url: PROV_SERVICE_BASEURL + 'activities/' + encodeURIComponent(record.get("runId")),
+          reader: {
+            root: 'activities',
+            totalProperty: 'totalCount'
+          },
+          simpleSortMode: true
+
+        });
+        sys.prune();
+        artifactStore.data.clear();
+        activityStore.data.clear();
+        activityStore.load({
+          callback: function() {
+            currentRun = record.get("runId")
+            Ext.getCmp('filtercurrent').enable();
+            Ext.getCmp('searchartifacts').enable();
+            Ext.getCmp('downloadscript').enable();
+
+
+
+
+          }
+
+        })
+
+        activityStore.on('load', onStoreLoad, this, {
+          single: true
+        });
+        currentRun = record.get("runId")
+        owner = userSN
+
+      }
+      
+      
+     
+    
+    }
+  }
+
+
+});
+
 
 // ComboBox with multiple selection enabled
 Ext.define('CF.view.metaCombo', {
@@ -264,6 +498,8 @@ function addMeta(url) {
 }
 
 
+var workflowgrid=Ext.create('CF.view.WorlflowSelection')
+
 Ext.define('CF.view.WorkflowOpenByRunID', {
     extend: 'Ext.form.Panel',
     // The fields
@@ -463,9 +699,11 @@ Ext.define('CF.view.WorkflowValuesRangeSearch', {
           workflowStore.getProxy().api.read = PROV_SERVICE_BASEURL + 'workflow/user/' + userSN + '?keys='+keys+"&maxvalues="+maxvalues+"&minvalues="+minvalues
         };
 
-
-
+         
+		//BUG: with a single load the grid doesn't synchronise when scrolled to the bottom
         workflowStore.load()
+        workflowStore.load()
+        //workflowStore.sync()
       }
     }]
   }
@@ -474,33 +712,18 @@ Ext.define('CF.view.WorkflowValuesRangeSearch', {
 
 
 
-var activityStore = Ext.create('CF.store.Activity');
 
-var artifactStore = Ext.create('CF.store.Artifact');
 
-var singleArtifactStore = Ext.create('CF.store.Artifact');
-
-var workflowStore = Ext.create('CF.store.ProvWorkflow');
-
-var workflowInputStore = Ext.create('CF.store.WorkflowInput');
+var workflowValuesRangeSearch = Ext.create('CF.view.WorkflowValuesRangeSearch')
+var workflowOpenByRunID = Ext.create('CF.view.WorkflowOpenByRunID')
 
 
 
-var action = Ext.create('Ext.Action', {
-  tooltip: 'Open Run',
-  text: 'Open Run',
-
-  handler: function(url) {
-
-
-    if (typeof workflowSel != "undefined") {
-      workflowSel.close();
-    }
-    workflowSel = Ext.create('Ext.window.Window', {
+var workflowSel = Ext.create('Ext.window.Window', {
       title: 'Workflows Runs',
       height: 530,
       width: 850,
-      closeAction: 'destroy',
+      closeAction: 'hide',
       layout: {
         type: 'vbox',
         align: 'stretch',
@@ -516,19 +739,31 @@ var action = Ext.create('Ext.Action', {
                     split: true
                 },
 
-                items: [Ext.create('CF.view.WorkflowValuesRangeSearch'),
-                		Ext.create('CF.view.WorkflowOpenByRunID')
+                items: [workflowValuesRangeSearch,
+                		workflowOpenByRunID
                 	]
                 },
-       		Ext.create('CF.view.WorlflowSelection')
+       		workflowgrid
       ]
 
     })
 
+var action = Ext.create('Ext.Action', {
+  tooltip: 'Open Run',
+  text: 'Open Run',
+
+  handler: function(url) {
+
+
+   /* if (typeof workflowSel != "undefined") {
+      var workflowStore = Ext.create('CF.store.ProvWorkflow');
+    } */
+
+
 
     workflowSel.show();
     workflowStore.getProxy().api.read = PROV_SERVICE_BASEURL + 'workflow/user/' + userSN
-    workflowStore.data.clear()
+    
     workflowStore.load()
   }
 });
@@ -660,229 +895,7 @@ var viewInputAction = Ext.create('Ext.Action', {
 
 
 
-Ext.define('CF.view.WorlflowSelection', {
 
-  width: 780,
-  disableSelection: true,
-
-  autoHeight: true,
-  extend: 'Ext.grid.Panel',
-
-
-  requires: [
-    'CF.store.Workflow',
-    'Ext.grid.plugin.BufferedRenderer'
-  ],
-  store: workflowStore,
-
-
-  /*   verticalScroller: {
-        xtype: 'paginggridscroller'
-    },*/
-
-
-
-  /* plugins: [{
-        ptype: 'bufferedrenderer'
-    }],*/
-
-
-  /* selModel: {
-        pruneRemoved: false
-    },
-*/
-  initComponent: function() {
-    Ext.apply(this, {
-      border: false,
-
-      loadMask: true,
-       
-      columns: [{
-          xtype: 'rownumberer',
-
-          sortable: false
-        },
-
-        {
-          header: 'Run ID',
-          dataIndex: 'runId',
-          flex: 5,
-          sortable: false
-        }, {
-          header: 'Workflow Name',
-          dataIndex: 'workflowName',
-          flex: 3,
-          sortable: false,
-          groupable: false
-        }, {
-          header: 'Description',
-          dataIndex: 'description',
-          flex: 3,
-          sortable: false,
-          field: {
-            xtype: 'textfield',
-            allowBlank: true,
-            maxLength: 50
-          }
-        }, {
-          header: 'Date',
-          dataIndex: 'date',
-          flex: 3,
-          sortable: false,
-          groupable: false
-        }, // custom mapping
-        {
-          xtype: 'actioncolumn',
-          width: 40,
-          tdCls: 'Delete',
-          items: [{
-            icon: localResourcesPath + '/img/delete-icon.png', // Use a URL in the icon config
-            tooltip: 'Delete',
-            handler: function(grid, rowIndex, colIndex) {
-              var rec = grid.getStore().getAt(rowIndex);
-
-              var storeClassName = Ext.getClassName(grid.getStore());
-              var tempStore = Ext.create(storeClassName, {
-                buffered: false
-              });
-              tempStore.add(rec);
-              var xx = tempStore.getAt(0)
-              messagebox = Ext.Msg.confirm('Remove Run', 'Are you sure?', function(button) {
-                if (button == 'yes') {
-
-
-                  var tempx = tempStore.getProxy()
-                  tempx.api.destroy = PROV_SERVICE_BASEURL + "workflow/delete/" + xx.get("runId");
-
-                  tempStore.remove(xx);
-
-                  tempStore.sync({
-                    success: function(args) {
-
-
-                      Ext.Ajax.request({
-                        url: deleteWorkflowURL,
-                        params: {
-                          "workflowId": xx.get('systemId')
-                        },
-                        success: function(response) {
-                          wfStore.load();
-                        },
-                        failure: function(response) {
-
-                        }
-                      })
-
-                      // workflowStore.data.clear()
-                      workflowStore.load()
-
-
-                    },
-
-                    faliure: function(args) {
-
-                      Ext.Msg.alert("Error", "Delete failed!");
-
-                    }
-
-                  })
-
-
-
-                }
-              })
-              messagebox.zIndexManager.bringToFront(messagebox);
-
-
-
-            }
-
-
-
-          }]
-        }
-      ],
-      flex: 1,
-      selType: 'cellmodel',
-      plugins: [
-        Ext.create('Ext.grid.plugin.CellEditing', {
-          clicksToEdit: 1,
-          listeners: {
-            'validateedit': function(c, e, eOpts) {
-              var r = e.rowIdx;
-              var sr = workflowStore.getAt(r);
-
-              sr.set(e.field, e.value);
-
-              workflowStore.load()
-            }
-          }
-        })
-      ]
-
-
-
-
-    });
-    this.callParent(arguments);
-
-    // store singleton selection model instance
-
-
-  },
-
-  viewConfig: {
-     enableTextSelection: true,
-    listeners: {
-   
-      itemclick: function(dv, record, item, index, e) {
-        workflowStore.sync()
-      },
-      itemdblclick: function(dataview, record, item, index, e) {
-
-        activityStore.setProxy({
-          type: 'ajax',
-          url: PROV_SERVICE_BASEURL + 'activities/' + encodeURIComponent(record.get("runId")),
-          reader: {
-            root: 'activities',
-            totalProperty: 'totalCount'
-          },
-          simpleSortMode: true
-
-        });
-        sys.prune();
-        artifactStore.data.clear();
-        activityStore.data.clear();
-        activityStore.load({
-          callback: function() {
-            currentRun = record.get("runId")
-            Ext.getCmp('filtercurrent').enable();
-            Ext.getCmp('searchartifacts').enable();
-            Ext.getCmp('downloadscript').enable();
-
-
-
-
-          }
-
-        })
-
-        activityStore.on('load', onStoreLoad, this, {
-          single: true
-        });
-        currentRun = record.get("runId")
-        owner = userSN
-
-      }
-      
-      
-     
-    
-    }
-  }
-
-
-});
 
 function onStoreLoad(store) {
   Ext.getCmp('viewworkflowinput').enable();
