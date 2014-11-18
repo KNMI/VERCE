ae3159b6c7000b7b1002e186efa49e7ca0dde9c7
Spinuso <Spinuso@e3b179e7-38d4-40e1-96b2-8a60d5dcc249>
2014-11-18 15:16:38 +0000

    inclusion of SPECFEM3D_CARTESIAN_202_DEV and Maule mesh and model
    git-svn-id: http://sisprojs.ipgp.fr/repos/verce/All/SA/SA3/Scientifc-Gateway/sg-trunk@4593 e3b179e7-38d4-40e1-96b2-8a60d5dcc249

--- liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/store/Solver.js
+++ liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/store/Solver.js
@@ -2,7 +2,12 @@ var solvers = [{
   "abbr": "SPECFEM3D_CARTESIAN_21447",
   "name": "SPECFEM3D_CARTESIAN_21447",
   "doc": "https://github.com/geodynamics/specfem3d/raw/devel/doc/USER_MANUAL/manual_SPECFEM3D_Cartesian.pdf"
-}];
+},
+{
+	  "abbr": "SPECFEM3D_CARTESIAN_202_DEV",
+	  "name": "SPECFEM3D_CARTESIAN_202_DEV",
+	  "doc": "https://github.com/geodynamics/specfem3d/raw/devel/doc/USER_MANUAL/manual_SPECFEM3D_Cartesian.pdf"
+	}];
 
 Ext.define('CF.store.Solver', {
   extend: 'Ext.data.Store',
