c33ee3ad72e69da614abeffcc73e4e5d045d73b4
Spinuso <Spinuso@e3b179e7-38d4-40e1-96b2-8a60d5dcc249>
2014-12-04 13:27:04 +0000

    deprecated specfem 201
    git-svn-id: http://sisprojs.ipgp.fr/repos/verce/All/SA/SA3/Scientifc-Gateway/sg-trunk@4647 e3b179e7-38d4-40e1-96b2-8a60d5dcc249

--- liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/store/Solver.js
+++ liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/store/Solver.js
@@ -1,13 +1,13 @@
-var solvers = [{
+var solvers = [/* deprecated {
   "abbr": "SPECFEM3D_CARTESIAN_21447",
   "name": "SPECFEM3D_CARTESIAN_21447",
   "doc": "https://github.com/geodynamics/specfem3d/raw/devel/doc/USER_MANUAL/manual_SPECFEM3D_Cartesian.pdf"
-}/*,
+},*/
 {
 	  "abbr": "SPECFEM3D_CARTESIAN_202_DEV",
 	  "name": "SPECFEM3D_CARTESIAN_202_DEV",
 	  "doc": "https://github.com/geodynamics/specfem3d/raw/devel/doc/USER_MANUAL/manual_SPECFEM3D_Cartesian.pdf"
-	}*/];
+	}];
 
 Ext.define('CF.store.Solver', {
   extend: 'Ext.data.Store',
