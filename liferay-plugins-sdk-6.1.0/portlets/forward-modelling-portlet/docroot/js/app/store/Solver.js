fe3c5785f56408ffbbd100b90bee108916e54e24
rafiq saleh <r.saleh@liverpool.ac.uk>
2017-07-28 14:48:10 +0100

    remove cartesian solver old version

--- liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/store/Solver.js
+++ liferay-plugins-sdk-6.1.0/portlets/forward-modelling-portlet/docroot/js/app/store/Solver.js
@@ -2,17 +2,16 @@ var solvers = [/* deprecated {
   "abbr": "SPECFEM3D_CARTESIAN_21447",
   "name": "SPECFEM3D_CARTESIAN_21447",
   "doc": "https://github.com/geodynamics/specfem3d/raw/devel/doc/USER_MANUAL/manual_SPECFEM3D_Cartesian.pdf"
-},*/
-     {
-	   "abbr": "SPECFEM3D_CARTESIAN_NEW",
-	   "name": "SPECFEM3D_CARTESIAN_NEW",
-	   "doc": "https://github.com/geodynamics/specfem3d/raw/devel/doc/USER_MANUAL/manual_SPECFEM3D_Cartesian.pdf"
-	 },
-	 {
+},{
 	  "abbr": "SPECFEM3D_CARTESIAN_202_DEV",
 	  "name": "SPECFEM3D_CARTESIAN_202_DEV",
 	  "doc": "https://github.com/geodynamics/specfem3d/raw/devel/doc/USER_MANUAL/manual_SPECFEM3D_Cartesian.pdf"
-	},
+	},*/
+     {
+	   "abbr": "SPECFEM3D_CARTESIAN",
+	   "name": "SPECFEM3D_CARTESIAN",
+	   "doc": "https://github.com/geodynamics/specfem3d/raw/devel/doc/USER_MANUAL/manual_SPECFEM3D_Cartesian.pdf"
+	 },	 
 	{
 		"abbr": "SPECFEM3D_GLOBE",
 		"name": "SPECFEM3D_GLOBE",
