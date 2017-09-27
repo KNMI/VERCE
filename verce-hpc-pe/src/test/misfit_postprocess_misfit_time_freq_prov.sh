0cdc926a4fa071255640c0ba1a58f19e27ef4c2f
aspinuso <spinuso@knmi.nl>
2017-09-27 18:47:54 +0200

    updated preprocessing and misfit workflows

--- verce-hpc-pe/src/test/misfit_postprocess_misfit_time_freq_prov.sh
+++ verce-hpc-pe/src/test/misfit_postprocess_misfit_time_freq_prov.sh
@@ -1,11 +1,11 @@
 cd ..
 export RUN_ID='testt_misfit'
 export STAGED_DATA=./
-export TEST=../test-resources/testfiles/
+export TEST=../../test-resources/testfiles/
 export USER_NAME=aspinuso
 export EVENT_PATH=$JSON_OUT
 export RUN_PATH=$JSON_OUT
-export PROV_PATH=prov_files/
+export PROV_PATH=../../test-resources/prov_files/
 export INPUT_FILE=$JSON_OUT/jsonout_run_specfem
 python -m dispel4py.new.processor simple  test.misfit_processing.misfit_processing_prov -f test/misfit_processing/misfit_time_frequency.json
 #mpirun-openmpi-mp -n 20 python -m dispel4py.new.processor mpi test.misfit_postprocessing.misfit_postprocess_prov -f test/misfit_postprocessing/misfit_time_frequency.json
